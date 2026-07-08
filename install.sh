9384a35d5e9d38993bd55770e752969ec11e6ef2
minfive <minfive@protonmail.com>
2026-07-08 20:41:13 +0200

    added set rofi power menu executeable to install.sh; changed power menu to only show shutdown, reboot and suspend

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cp -r .config ~/
 cp -r .themes ~/
 
 chmod +x ~/.config/rofi/scripts/rofi-web-search.py
+chmod +x ~/.config/rofi/scripts/rofi-power-menu
 chmod +x ~/.config/waybar/scripts/mullvad-status.sh
 
 #Install fonts
